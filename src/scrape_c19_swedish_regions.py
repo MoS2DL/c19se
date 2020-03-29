ac09993085d0574b8ac89948c50276a8991705a4
Daniel Lindroth <daniel.lindroth@knowit.se>
2020-03-29 22:05:37 +0200

    Add script for webscraping data for Swedish regions ad c19.se

---
+++ src/scrape_c19_swedish_regions.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+import os
+import pandas as pd
+import pathlib
+import yaml
+
+from utilities_c19_swedish_regions import (
+    scrape_data_c19_sweden_region,
+    generate_csv
+)
+
+
+def main():
+    with open('configuration/swedish_regions.yml') as f:
+        configuration = yaml.load(f, Loader=yaml.FullLoader)
+
+    now = datetime.now()
+    date = f"{now.year}-{now.month:02d}-{now.day:02d}"
+    NOW = f"{date}.{now.hour:02d}:{now.minute:02d}"
+
+    outdir = pathlib.Path.cwd() / "data" / "scraped_c19" / NOW
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir)
+
+    regions = list(configuration.keys())
+    for region in regions:
+        url = configuration[region]['url']
+        file_prefix = configuration[region]['file_prefix']
+        data = scrape_data_c19_sweden_region(url)
+
+        generate_csv(data, file_prefix, outdir)
+
+
+if __name__ == "__main__":
+
+    main()
---
+++ src/utilities_c19_swedish_regions.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+from datetime import datetime
+import pandas as pd
+import pathlib
+import requests
+
+
+def scrape_data_c19_sweden_region(url):
+    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+    scripts = soup.find_all("script")
+    script = scripts[-1]
+
+    script_split_newline = script.text.split('\n')
+    data_list = []
+    for line in script_split_newline:
+        if 'data' in line:
+            data = [int(x) for x in line[7:-1].split(',')]
+            data_list.append(data)
+
+    data = {
+        'confirmed': data_list[0],
+        'fatalities': data_list[2],
+        'ICU': data_list[3]
+    }
+
+    return data
+
+
+def generate_csv(data, file_prefix, outdir):
+
+    now = datetime.now()
+    date = f"{now.year}-{now.month:02d}-{now.day:02d}"
+
+    START_DATE = '2020-03-02'
+    END_DATE = date
+
+    dates = pd.date_range(start=START_DATE, end=END_DATE)
+
+
+    NOW = f"{date}.{now.hour:02d}:{now.minute:02d}"
+    filename = f"{file_prefix}_{NOW}.csv"
+    filepath = outdir / filename
+
+    df = pd.DataFrame(
+        data=data,
+        index=dates,
+        dtype='Int64'
+    )
+
+    df.to_csv(filepath, index=True, index_label='date')
+
+
+if __name__ == "__main__":
+
+    url = 'https://c19.se/Sweden/Stockholm'
+    print(scrape_data_c19_sweden_region(url))
